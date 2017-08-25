c8e2b5037aa74e9b1cde1b7c3bde2885f42b5e85
Justin <jusjef@amazon.com>
2017-08-25 16:46:24 -0700

    Day-1 facts updates
    Signed-off-by: Justin <jusjef@amazon.com>

--- labs/Day-1/5-number-facts (Custom Slots)/src/index.js
+++ labs/Day-1/5-number-facts (Custom Slots)/src/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var Alexa = require('alexa-sdk');
+const Alexa = require('alexa-sdk');
 
 //=========================================================================================================================================
 //TODO: The items below this comment need your attention.
@@ -7,18 +7,18 @@ var Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = undefined;
+const APP_ID = undefined;
 
-var SKILL_NAME = "Number Facts";
-var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a number fact, or, you can say exit... What can I help you with?";
-var HELP_REPROMPT = "What can I help you with?";
-var STOP_MESSAGE = "Goodbye!";
+const SKILL_NAME = "Number Facts";
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = "You can say tell me a number fact, or, you can say exit... What can I help you with?";
+const HELP_REPROMPT = "What can I help you with?";
+const STOP_MESSAGE = "Goodbye!";
 
 //=========================================================================================================================================
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
-var data = [
+const data = [
   "7 is the maximum number of times a letter-sized paper can be folded in half",
   "11 is the number of players in a football team",
   "37 is the normal human body temperature in degrees Celsius",
@@ -47,7 +47,10 @@ var handlers = {
         var factIndex = Math.floor(Math.random() * factArr.length);
         var randomFact = factArr[factIndex];
         var speechOutput = GET_FACT_MESSAGE + randomFact;
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+
+        this.response.cardRenderer(SKILL_NAME, randomFact);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
     },
     'GetNewFactAboutIntent': function () {
         var theNumber = this.event.request.intent.slots.number.value;
@@ -58,19 +61,24 @@ var handlers = {
         httpGet(myRequest,  (myResult) => {
                 console.log("sent     : " + myRequest);
                 console.log("received : " + myResult);
-                this.emit(':tell', myResult );
+                this.response.speak(myResult);
+                this.emit(':responseReady');
             });
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = HELP_MESSAGE;
         var reprompt = HELP_REPROMPT;
-        this.emit(':ask', speechOutput, reprompt);
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', STOP_MESSAGE);
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', STOP_MESSAGE);
+      this.response.speak(STOP_MESSAGE);
+      this.emit(':responseReady');
     }
 };
 
