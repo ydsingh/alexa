03fef0662d97a4ed4c71162910378353050dc4a5
Greg Bulmash <bulmashg@amazon.com>
2020-02-08 10:02:34 +0530

    adding ASL notices

--- feature-demos/smapi-sdk-node-demo/tokens.js
+++ feature-demos/smapi-sdk-node-demo/tokens.js
@@ -1,3 +1,7 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+// Licensed under the Amazon Software License  http://aws.amazon.com/asl/
+
 // A simple object to hold your authentication information
 
 module.exports = {
